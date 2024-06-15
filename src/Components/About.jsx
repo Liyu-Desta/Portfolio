95f33fde9243631542694b721ea3f1e0383458ff
liyu <liyudesta22@gmail.com>
2024-06-15 20:57:57 +0300

    portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a software engineer working both website development and mobile application.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I enjoy creating interactive design in Figma for my projects and I like creative way of thinking for building problem solving websites.";
 
 const About = () => {
   return (
